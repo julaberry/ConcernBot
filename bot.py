32eb281cfe599ffe1903363626e55562b2df649c
Jennifer <chrystamofdem@gmail.com>
2021-11-07 17:47:02 -0800

    Bug fixing

--- bot.py
+++ bot.py
@@ -49,20 +49,20 @@ async def on_message(message):
 	#if message.author.id not in concernedonlyexceptions:
 	msgemotes = re.findall(r'<a?:\w*:\d*>', message.content)
 	msgemotes = [int(e.split(':')[1].replace('>', '')) for e in msgemotes]
-	illegal = all(item in serveremotes for item in msgemotes)
-
-	if illegal:
-		await message.delete(message)
+	for x in msgemotes:
+		if x not in serveremotes:
+			await message.delete()
+			return
 
 	if "tm" in message.content and message.author.id in concernedonlyexceptions:
 		trollmode = not trollmode
 
 	if trollmode and message.author.id == 278396296430092289:
 		if random.randint(0,10) < 3:
-			await message.delete(message)
+			await message.delete()
 
 	if "mustard is great" in message.content and message.author.id != 278396296430092289:
-		await message.delete(message)
+		await message.delete()
 		role = discord.utils.get(message.guild.roles, name="fake admin role don't give this to anyone")
 		await message.author.add_roles(role)
 
