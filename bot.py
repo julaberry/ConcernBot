9a2dbed9a5e40d3c2ef8b4163ef5c7edf71077af
Jennifer <chrystamofdem@gmail.com>
2021-11-07 17:37:46 -0800

    Added several new functions

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
+import re
 import discord
 import json
+import random
 from discord.utils import get
 
 intents = discord.Intents.default()
@@ -19,6 +21,12 @@ with open("config.json") as conf:
 TOKEN = 'Nzg4NTU1OTQ1MTc2OTg5NzM2.X9lN8w.2aWpqXg-6pPZiUJOfY3f1R7paKo'
 
 emotes = [785953585594499112,785953620071940137,785953661271801917,785953681383620628,785953711305916497,785953732159864923,785953770474438666,785953808303652875,785953830336200784,785953848996528168,785955049557065738,785955086072938526,785955110949617694,785955128440127528,785955147611111466,785955165190094868,785955194646167612,785955217380081715,785955247960490084,785955268659380285]
+otheremotes = [786349991874527242,785965937790681149,896310029911724044]
+serveremotes = emotes+otheremotes
+
+concernedonlyexceptions = [379145843200229379, 201715919439790080, 328725093071192066, 243744413061218304]
+
+trollmode = False
 
 @client.event
 async def on_ready():
@@ -37,14 +45,28 @@ async def on_member_join(member):
 async def on_message(message):
 	if message.author == client.user:
 		return
-	'''
-	emotes = []
-	for i in range(1,22):
-		if i == 18:
-			continue
-		s = "concerned" + str(i)
-		emotes.append(s)
-	'''
+	
+	#if message.author.id not in concernedonlyexceptions:
+	msgemotes = re.findall(r'<a?:\w*:\d*>', msg.content)
+    msgemotes = [int(e.split(':')[1].replace('>', '')) for e in custom_emojis]
+    illegal = all(item in serveremotes for item in msgemotes)
+
+    if illegal:
+    	await message.delete(message)
+
+    if "tm" in message.content and message.author.id in concernedonlyexceptions:
+    	trollmode = not trollmode
+
+    if trollmode and message.author.id == 278396296430092289:
+    	if random.randint(0,10) < 3:
+    		await message.delete(message)
+
+	if "mustard is great" in message.content and message.author.id != 278396296430092289:
+        await message.delete(message)
+        role = discord.utils.get(message.guild.roles, name="fake admin role don't give this to anyone")
+        await message.author.add_roles(role)
+
+
 	try:
 		for emotename in emotes:
 			#emote = get(message.guild.emojis, name=emotename)
