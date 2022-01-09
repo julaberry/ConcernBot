c8a6fa3d226c2e55c9a0858902145b766a287f0b
Jenn <chrystamofdem@gmail.com>
2022-01-09 13:49:24 -0800

    Converted for self host use.

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import re
 import discord
-import json
+from dotenv import load_dotenv
+import os
 import random
 import emoji
 from discord.utils import get
@@ -18,14 +19,38 @@ with open("config.json") as conf:
 	emotes = configs["reacts"]
 '''
 
+load_dotenv("config.env")
+TOKEN = os.getenv("TOKEN")
+REACTS = os.getenv("REACTS")
+EMOTES = os.getenv("EMOTES")
+SUPERS = os.getenv("SUPERS")
+TROLLS = os.getenv("TROLLS")
 
-TOKEN = 'Nzg4NTU1OTQ1MTc2OTg5NzM2.X9lN8w.2aWpqXg-6pPZiUJOfY3f1R7paKo'
 
-emotes = [785953585594499112,785953620071940137,785953661271801917,785953681383620628,785953711305916497,785953732159864923,785953770474438666,785953808303652875,785953830336200784,785953848996528168,785955049557065738,785955086072938526,785955110949617694,785955128440127528,785955147611111466,785955165190094868,785955194646167612,785955217380081715,785955247960490084,785955268659380285]
-otheremotes = [786349991874527242,785965937790681149,896310029911724044, 915390704832884756, 920374139523563590]
-serveremotes = emotes+otheremotes
+emotes = []
+otheremotes = []
+serveremotes = []
+concernedonlyexceptions =[]
+trolledusers = []
 
-concernedonlyexceptions = [379145843200229379, 201715919439790080, 328725093071192066, 243744413061218304, 284528757300264961, 705247998439063562, 272274466107686913]
+
+def refresh():
+	global emotes
+	global otheremotes
+	global serveremotes
+	global concernedonlyexceptions
+	global trolledusers
+	with open(REACTS) as f:
+		emotes = [int(line.rstrip()) for line in f]
+	with open(EMOTES) as f:
+		otheremotes = [int(line.rstrip()) for line in f]
+	serveremotes = emotes + otheremotes
+	with open(SUPERS) as f:
+		concernedonlyexceptions = [int(line.rstrip()) for line in f]
+	with open(TROLLS) as f:
+		trolledusers = [int(line.rstrip()) for line in f]
+
+refresh()
 
 trollmode = False
 exceptionmode = False
@@ -46,6 +71,12 @@ async def on_member_join(member):
 @client.event
 async def on_message(message):
 	if message.author == client.user:
+		return
+
+	if "!refresh" in message.content and message.author.id in concernedonlyexceptions:
+		refresh()
+		print("Refreshed emotes and exceptions.")
+		await message.delete()
 		return
 
 	global exceptionmode
@@ -70,11 +101,11 @@ async def on_message(message):
 		trollmode = not trollmode
 		print("Troll Mode: ", trollmode)
 
-	if trollmode and message.author.id == 278396296430092289:
+	if trollmode and message.author.id in trolledusers:
 		if random.randint(0,10) < 6:
 			await message.delete()
 
-	if "mustard is great" in message.content and message.author.id != 278396296430092289:
+	if "mustard is great" in message.content and message.author.id not in trolledusers:
 		await message.delete()
 		role = discord.utils.get(message.guild.roles, name="fake admin role don't give this to anyone")
 		await message.author.add_roles(role)
@@ -86,8 +117,8 @@ async def on_message(message):
 			emote = client.get_emoji(emotename)
 			if (emote != None):
 				await message.add_reaction(emote)
-	except:
-		pass
+	except Exception as e:
+		print(e)
 	
 
 
